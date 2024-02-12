59ed9936adc33a6af9fbfbbb34dba41a3a33e8c5
DmitryRomankin <156576989+DmitryRomankin@users.noreply.github.com>
2024-02-12 22:20:26 +0300

    Add files via upload

---
+++ ClientApplication/Authentication.java
@@ -0,0 +1,23 @@
+package ClientApplication;
+
+import Core.UserProvider;
+import Models.User;
+
+public class Authentication {
+    /**
+     * Метод производит аутентификацию
+     *
+     * @param userProvider
+     * @param login
+     * @param passHash
+     * @return
+     */
+    public static User authentication(UserProvider userProvider, String login, int passHash) {
+        var client = userProvider.getClientByName(login);
+        if (client.getHashPassword() == passHash) {
+            return client;
+        } else {
+            throw new RuntimeException("Authentication fail");
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+import ClientApplication.Start;
+import Services.CashRepository;
+
+@SuppressWarnings("unused")
+public class Main {
+    public static void main(String[] args) {
+        Start start = new Start();
+        start.runLoginRegisterMenu();;
+    }
+}
